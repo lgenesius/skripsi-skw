d72757ecaac5f5b6374a5d90ebf0719f657b185f
Luis Genesius <luis.huang321@gmail.com>
2021-12-11 07:33:42 +0700

    add skripsi xcodeproj

---
+++ SkripsiSKW/SkripsiSKW/SkripsiSKWApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkripsiSKWApp.swift
+//  SkripsiSKW
+//
+//  Created by Luis Genesius on 11/12/21.
+//
+
+import SwiftUI
+
+@main
+struct SkripsiSKWApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
