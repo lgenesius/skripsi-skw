c3b7c0084345d2629464c8156c281c865ec87164
Luis Genesius <luis.huang321@gmail.com>
2021-12-20 14:22:29 +0700

    add date text and circle in challenges view

--- SkripsiSKW/SkripsiSKW/Challenges/Views/ChallengesView.swift
+++ SkripsiSKW/SkripsiSKW/Challenges/Views/ChallengesView.swift
@@ -11,11 +11,26 @@ struct ChallengesView: View {
     @EnvironmentObject var sessionVM: SessionViewModel
     
     var body: some View {
-        Button {
-            sessionVM.logout()
-        } label: {
-            Text("Logout")
+        VStack {
+            HStack {
+                Text("Monday, October 25")
+                    .foregroundColor(Color.notYoCheese)
+                    .font(Font.system(size: 17))
+                Spacer()
+                Circle()
+                    .frame(width: 36, height: 36)
+                    .foregroundColor(Color.notYoCheese)
+            }
+            .padding()
+            Button {
+                sessionVM.logout()
+            } label: {
+                Text("Logout")
+            }
+            
+            Spacer()
         }
+        .navigationBarHidden(true)
     }
 }
 
