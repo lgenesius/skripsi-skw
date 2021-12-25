e8b7d88e216121077fc0ebc9eac2d44f84f079f4
Luis Genesius <luis.huang321@gmail.com>
2021-12-25 15:28:06 +0700

    add devicerotationviewmodifier

---
+++ SkripsiSKW/SkripsiSKW/Utils/Components/Modifiers/DeviceRotationViewModifier.swift
@@ -0,0 +1,20 @@
+//
+//  DeviceRotationViewModifier.swift
+//  SkripsiSKW
+//
+//  Created by Luis Genesius on 25/12/21.
+//
+
+import SwiftUI
+
+struct DeviceRotationViewModifier: ViewModifier {
+    let action: (UIDeviceOrientation) -> Void
+
+    func body(content: Content) -> some View {
+        content
+            .onAppear()
+            .onReceive(NotificationCenter.default.publisher(for: UIDevice.orientationDidChangeNotification)) { _ in
+                action(UIDevice.current.orientation)
+            }
+    }
+}
