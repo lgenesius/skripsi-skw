adbdb60a2381e97461a357df0d49eee91fa4ac41
Luis Genesius <luis.huang321@gmail.com>
2021-12-28 15:31:08 +0700

    modified workoutrotationview

--- SkripsiSKW/SkripsiSKW/Workout/Rotation/Views/WorkoutRotationView.swift
+++ SkripsiSKW/SkripsiSKW/Workout/Rotation/Views/WorkoutRotationView.swift
@@ -8,13 +8,58 @@
 import SwiftUI
 
 struct WorkoutRotationView: View {
+    @Binding var isOrientationPresent: Bool
+    var completion: (() -> Void)
+    
+    @State private var orientation = UIDeviceOrientation.unknown
+    
     var body: some View {
-        Text("Hello, World!")
+        ZStack {
+            Color.black.opacity(0.7)
+                .ignoresSafeArea()
+            
+            VStack {
+                Spacer()
+                Text("Rotate your Phone Screen")
+                    .modifier(TextModifier(
+                        color: .white,
+                        size: 30,
+                        weight: .bold)
+                    )
+                    .lineLimit(2)
+                    .multilineTextAlignment(.center)
+                    .padding()
+                Spacer()
+                Image("rotate-landscape")
+                    .frame(width: 110, height: 110)
+                Spacer()
+                Spacer()
+                RoundedButton(title: "Done") {
+                    isOrientationPresent = false
+                }
+                .allowsHitTesting(!orientation.isLandscape ? false: true)
+                .opacity(!orientation.isLandscape ? 0.5: 1)
+            }
+            .padding()
+        }
+        .onRotate { newOrientation in
+            orientation = newOrientation
+        }
     }
 }
 
 struct WorkoutRotationView_Previews: PreviewProvider {
     static var previews: some View {
-        WorkoutRotationView()
+        if #available(iOS 15.0, *) {
+            WorkoutRotationView(isOrientationPresent: .constant(true), completion: {
+                
+            })
+                .previewInterfaceOrientation(.landscapeLeft)
+        } else {
+            // Fallback on earlier versions
+            WorkoutRotationView(isOrientationPresent: .constant(true), completion: {
+                
+            })
+        }
     }
 }
