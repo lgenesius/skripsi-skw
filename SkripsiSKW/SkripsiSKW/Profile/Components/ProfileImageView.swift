10ea63183c1b3f633ea22651fda7efbf0b0bb7f0
Luis Genesius <luis.huang321@gmail.com>
2022-01-09 19:24:16 +0700

    add conditional for force unwrap in profile image view

--- SkripsiSKW/SkripsiSKW/Profile/Components/ProfileImageView.swift
+++ SkripsiSKW/SkripsiSKW/Profile/Components/ProfileImageView.swift
@@ -15,62 +15,64 @@ struct ProfileImageView: View {
     var height: CGFloat
     
     var body: some View {
-        if #available(iOS 15.0, *) {
-            if !currentUser!.profileImageUrl.isEmpty {
-                WebImage(url: URL(string: currentUser!.profileImageUrl))
-                    .resizable()
-                    .indicator(.activity)
-                    .scaledToFill()
-                    .frame(width: width, height: height)
-                    .clipShape(Circle())
-                    .overlay(alignment: .bottomTrailing) {
-                        Image(systemName: "pencil.circle.fill")
-                            .foregroundColor(.white)
-                            .scaleEffect(2)
-                            .opacity(userId == nil ? 1: 0)
-                    }
+        if currentUser != nil {
+            if #available(iOS 15.0, *) {
+                if !currentUser!.profileImageUrl.isEmpty {
+                    WebImage(url: URL(string: currentUser!.profileImageUrl))
+                        .resizable()
+                        .indicator(.activity)
+                        .scaledToFill()
+                        .frame(width: width, height: height)
+                        .clipShape(Circle())
+                        .overlay(alignment: .bottomTrailing) {
+                            Image(systemName: "pencil.circle.fill")
+                                .foregroundColor(.white)
+                                .scaleEffect(2)
+                                .opacity(userId == nil ? 1: 0)
+                        }
+                } else {
+                    Image(systemName: "person.circle.fill")
+                        .resizable()
+                        .scaledToFit()
+                        .foregroundColor(.white)
+                        .frame(width: width, height: height)
+                        .overlay(alignment: .bottomTrailing) {
+                            Image(systemName: "pencil.circle.fill")
+                                .foregroundColor(.white)
+                                .scaleEffect(2)
+                                .opacity(userId == nil ? 1: 0)
+                        }
+                }
             } else {
-                Image(systemName: "person.circle.fill")
-                    .resizable()
-                    .scaledToFit()
-                    .foregroundColor(.white)
-                    .frame(width: width, height: height)
-                    .overlay(alignment: .bottomTrailing) {
-                        Image(systemName: "pencil.circle.fill")
-                            .foregroundColor(.white)
-                            .scaleEffect(2)
-                            .opacity(userId == nil ? 1: 0)
-                    }
-            }
-        } else {
-            // Fallback on earlier versions
-            if !currentUser!.profileImageUrl.isEmpty {
-                WebImage(url: URL(string: currentUser!.profileImageUrl))
-                    .resizable()
-                    .indicator(.activity)
-                    .scaledToFill()
-                    .frame(width: width, height: height)
-                    .clipShape(Circle())
-                    .overlay(
-                        Image(systemName: "pencil.circle.fill")
-                            .foregroundColor(.white)
-                            .scaleEffect(2)
-                            .opacity(userId == nil ? 1: 0),
-                        alignment: .bottomTrailing
-                    )
-            } else {
-                Image(systemName: "person.circle.fill")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: width, height: height)
-                    .foregroundColor(.white)
-                    .overlay(
-                        Image(systemName: "pencil.circle.fill")
-                            .foregroundColor(.white)
-                            .scaleEffect(2)
-                            .opacity(userId == nil ? 1: 0),
-                        alignment: .bottomTrailing
-                    )
+                // Fallback on earlier versions
+                if !currentUser!.profileImageUrl.isEmpty {
+                    WebImage(url: URL(string: currentUser!.profileImageUrl))
+                        .resizable()
+                        .indicator(.activity)
+                        .scaledToFill()
+                        .frame(width: width, height: height)
+                        .clipShape(Circle())
+                        .overlay(
+                            Image(systemName: "pencil.circle.fill")
+                                .foregroundColor(.white)
+                                .scaleEffect(2)
+                                .opacity(userId == nil ? 1: 0),
+                            alignment: .bottomTrailing
+                        )
+                } else {
+                    Image(systemName: "person.circle.fill")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: width, height: height)
+                        .foregroundColor(.white)
+                        .overlay(
+                            Image(systemName: "pencil.circle.fill")
+                                .foregroundColor(.white)
+                                .scaleEffect(2)
+                                .opacity(userId == nil ? 1: 0),
+                            alignment: .bottomTrailing
+                        )
+                }
             }
         }
     }
